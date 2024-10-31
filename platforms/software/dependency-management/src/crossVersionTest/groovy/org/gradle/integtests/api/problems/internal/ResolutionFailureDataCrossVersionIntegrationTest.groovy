27ec21fa27e5ed4d8ef64b99205db3dcce01c619
Donát Csikós <donat@gradle.com>
2024-10-31 11:30:08 +0100

    Fix test

--- platforms/software/dependency-management/src/crossVersionTest/groovy/org/gradle/integtests/api/problems/internal/ResolutionFailureDataCrossVersionIntegrationTest.groovy
+++ platforms/software/dependency-management/src/crossVersionTest/groovy/org/gradle/integtests/api/problems/internal/ResolutionFailureDataCrossVersionIntegrationTest.groovy
@@ -33,7 +33,8 @@ import org.gradle.tooling.events.problems.internal.GeneralData
 @TargetGradleVersion(">=8.11")
 @ToolingApiVersion(">=8.11")
 class ResolutionFailureDataCrossVersionIntegrationTest extends ToolingApiSpecification {
-    def "can supply ResolutionFailureData"() {
+    @ToolingApiVersion(">=8.11 <8.12")
+    def "can supply ResolutionFailureData  (Tooling API client [8.11,8.12)"() {
         given:
         withReportProblemTask """
             TestResolutionFailure failure = new TestResolutionFailure()
@@ -47,6 +48,32 @@ class ResolutionFailureDataCrossVersionIntegrationTest extends ToolingApiSpecifi
         when:
         List<GeneralData> failureData = runAndGetProblems().collect { ProblemEvent problem ->
             problem.additionalData as GeneralData
+        }
+
+        then:
+        failureData.size() >= 1 // Depending on Java version, we might get a Java version test execution failure first, so just check the last one
+        failureData.last().asMap.tap { Map d ->
+            assert d.problemId == "UNKNOWN_RESOLUTION_FAILURE"
+            assert d.requestTarget == "test failure"
+            assert d.problemDisplayName == "Unknown resolution failure"
+        }
+    }
+
+    @ToolingApiVersion(">=8.12")
+    def "can supply ResolutionFailureData (Tooling API client >= 8.12)"() {
+        given:
+        withReportProblemTask """
+            TestResolutionFailure failure = new TestResolutionFailure()
+
+            getProblems().getReporter().reporting {
+                it.id("id", "shortProblemMessage")
+                .additionalData(ResolutionFailureDataSpec.class, data -> data.from(failure))
+            }
+        """
+
+        when:
+        List<GeneralData> failureData = runAndGetProblems().collect { ProblemEvent event ->
+            event.problem.additionalData as GeneralData
         }
 
         then:
